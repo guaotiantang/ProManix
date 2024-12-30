e391d614f03fafeb173a1dcc4812a3e357626874
guaotiantang <guaotiantang@outlook.com>
2024-12-30 15:33:12 +0800

    Update MroParser

--- MParser/nodes/ParserNode/TaskProcess.py
+++ MParser/nodes/ParserNode/TaskProcess.py
@@ -13,6 +13,9 @@ from aiomultiprocess import Process
 from HttpClient import HttpClient
 from config import BACKEND_URL, NDS_GATEWAY_URL
 
+import pandas as pd
+from lxml import etree
+
 
 class TaskProcess:
     def __init__(self, process_count: int = 2):
@@ -145,6 +148,7 @@ async def sub_process(pid, queue, status, lock, shutdown_event):
             continue
 
 
+# noinspection HttpUrlsUsage,PyBroadException
 async def parse_task(task_data: Dict[str, Any], backend_client: HttpClient):
     """处理单个任务的协程"""
     try:
@@ -181,10 +185,10 @@ async def parse_task(task_data: Dict[str, Any], backend_client: HttpClient):
                         for file_name in zip_file.namelist():
                             with zip_file.open(file_name) as f:
                                 content = f.read()
-                                if task_data.get("DataType", None) == "MRO" and file_name.endswith(".xml"):
-                                    await parse_mro_data(content)
-                                elif task_data.get("DataType", None) == "MDT" and file_name.endswith(".csv"):
-                                    await parse_mdt_data(content)
+                                if task_data.get("DataType", "").upper() == "MRO" and file_name.lower().endswith(".xml"):
+                                    await parse_mro_data(task_data, content)
+                                elif task_data.get("DataType", "").upper() == "MDT" and file_name.lower().endswith(".csv"):
+                                    await parse_mdt_data(task_data, content)
                                 else:
                                     raise Exception("No parser type")
 
@@ -214,11 +218,97 @@ async def parse_task(task_data: Dict[str, Any], backend_client: HttpClient):
         )
 
 
-async def parse_mro_data(data):
-    print("parse mro data length: ", len(data))
-    # TODO: 解析数据
+async def parse_mro_data(task_data, content):
+    """解析 MRO 数据"""
+    try:
+        result = []
+        
+        # 定义需要检查的字段
+        smr_check = [
+            "MR_LteScEarfcn", "MR_LteScPci", "MR_LteScRSRP",
+            "MR_LteNcEarfcn", "MR_LteNcPci", "MR_LteNcRSRP"
+        ]
+        
+        
+        tree = etree.fromstring(content)
+        LteScENBID = tree.find('.//eNB').attrib['id'] # 提取eNodeBID
+        
+        for measurement in tree.findall('.//measurement'):
+            smr_content = measurement.find('smr').text.strip()
+            smr_content = smr_content.replace('MR.', 'MR_')
+            smr_fields = smr_content.split()
+            data = []
+
+            if not set(smr_check).issubset(set(smr_fields)):
+                continue # 检查必要字段是否存在，当不存在时跳过该measurement节点
+                
+            headers = ["MR_LteScENBID"] + smr_check  # 字段列表(添加MR_LteScENBID)
+            smr_values = {x: i for i, x in enumerate(smr_fields) if x in smr_check}  # 字段索引映射
+            max_field_num = smr_values[max(smr_values, key=smr_values.get)]  # 找出所需字段中最后的索引位置
+            for obj in measurement.findall('object'):  # 遍历每个measurement下的object元素
+                for v in obj.findall('v'):  # 遍历每个object下的v元素（<v></v>）
+                    values = v.text.strip().split()  # 分割v元素内的文本内容
+                    if len(values) >= max_field_num: # 如果值的数量不够，跳过这条记录
+                        # 构建一行数据：[LteScENBID] + [对应位置的测量值]
+                        row_data = [LteScENBID] + [values[smr_values[x]] for x in headers[1:]]
+                        if 'NIL' not in row_data:
+                            data.append(row_data)  # 如果数据中没有NIL（无效值），则添加到数据列表中
+
+            df = pd.DataFrame(data, columns=headers)  # 将数据转换为DataFrame
+        
+            # 数据类型转换和计算
+            df[[
+                'MR_LteScPci', 'MR_LteScRSRP',
+                'MR_LteNcPci', 'MR_LteNcRSRP'
+            ]] = df[[
+                'MR_LteScPci', 'MR_LteScRSRP',
+                'MR_LteNcPci', 'MR_LteNcRSRP'
+            ]].astype(int)  # 将PCI和RSRP字段转换为整数类型
+        
+            # 计算同频6db和MOD3采样点数
+            df['MR_LteFCIn6db'] = ((df['MR_LteScEarfcn'] == df['MR_LteNcEarfcn']) &
+                                   (df['MR_LteScRSRP'] - df['MR_LteNcRSRP'] <= 6)).astype(int)
+
+            # 计算MOD3采样点数
+            df['MR_LTEMod3'] = ((df['MR_LteScEarfcn'] == df['MR_LteNcEarfcn']) &
+                               (df['MR_LteScPci'] % 3 == df['MR_LteNcPci'] % 3) &
+                               (df['MR_LteScRSRP'] - df['MR_LteNcRSRP'] <= 3) &
+                               (df['MR_LteScRSRP'] >= 30)).astype(int)
+
+            # 数值转换 - 将字符串转换为数值，如果无法转换则设置为NaN
+            for col in smr_check:
+                df[col] = pd.to_numeric(df[col], errors='coerce')
+            
+            # 数据分组统计 - 按指定字段分组，统计每组的和以及平均值
+            grouped = df.groupby(
+                ["MR_LteScENBID", "MR_LteScEarfcn", "MR_LteScPci", "MR_LteNcEarfcn", "MR_LteNcPci"]
+            ).agg(
+                MR_LteScSPCount=pd.NamedAgg(column="MR_LteScRSRP", aggfunc='count'),
+                MR_LteScRSRPAvg=pd.NamedAgg(column="MR_LteScRSRP", aggfunc=lambda x: x.mean()),
+                MR_LteNcSPCount=pd.NamedAgg(column="MR_LteNcRSRP", aggfunc='count'),
+                MR_LteNcRSRPAvg=pd.NamedAgg(column="MR_LteNcRSRP", aggfunc=lambda x: x.mean()),
+                MR_LteCC6Count=pd.NamedAgg(column="MR_LteFCIn6db", aggfunc='sum'),
+                MR_LteMOD3Count=pd.NamedAgg(column="MR_LTEMod3", aggfunc='sum')
+            ).reset_index()
+        
+            # 添加DataTime时间字段(15分钟粒度文件时间)
+            grouped['DataTime'] = pd.to_datetime(task_data.get('FileTime'))
+        
+            # 类型转换，确保与数据库类型一致
+            grouped['MR_LteScENBID'] = grouped['MR_LteScENBID'].astype('int8')
+            grouped['MR_LteScEarfcn'] = grouped['MR_LteScEarfcn'].astype('int32')
+            grouped['MR_LteScPci'] = grouped['MR_LteScPci'].astype('int8')
+            grouped['MR_LteNcEarfcn'] = grouped['MR_LteNcEarfcn'].astype('int32')
+            grouped['MR_LteNcPci'] = grouped['MR_LteNcPci'].astype('int32')
+
+            result.append(grouped.to_dict('records'))  # 将处理后的数据添加到结果中
+        return result
+        
+    except Exception as e:
+        print("MRO Parse Error:", str(e))
+        raise Exception(f"MRO Parse Error: {str(e)}")
 
 
-async def parse_mdt_data(data):
-    print("parse mdt data length: ", len(data))
+async def parse_mdt_data(task, data):
+    print(task, "parse mdt data length: ", len(data))
     # TODO: 解析数据
